874f560b7c9c29ebd4543a1ea83afe2a8b5f49de
JobsonOliveira <jobsonoliveira007@gmail.com>
2023-07-21 22:20:47 -0300

    Implementação de senha para a tela de login

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,8 +1,15 @@
 document.querySelector('#entrar').addEventListener('click', () => {
-    let aparecer = document.querySelector('#bemVindo');
-    aparecer.show();
-    setTimeout(()=> {
-        aparecer.close();
-        window.open('../HTML/paginaPrincipal.html','_self');
-    }, '1000');
+    let login = document.querySelector("#login");
+    let senha = document.querySelector("#senha");
+
+    if ((login.value == "Teste") && (senha.value == "Usuario123")) {
+        let aparecer = document.querySelector('#bemVindo');
+        aparecer.show();
+        setTimeout(()=> {
+            aparecer.close();
+            window.open('../HTML/paginaPrincipal.html','_self');
+        }, '1000');
+    }else{
+        alert("Usuário ou senha inválidos!");
+    }
 });
